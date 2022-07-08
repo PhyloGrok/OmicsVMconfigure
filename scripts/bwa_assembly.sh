fe2092990e84c0e8a61fe11cea2f60d870487121
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2022-07-08 12:35:12 -0400

    Update bwa_assembly.sh

--- scripts/bwa_assembly.sh
+++ scripts/bwa_assembly.sh
@@ -1,8 +1,8 @@
 ## Index the reference genome
 bwa index data/ref_genome/Halobacterium.fna
 
-## Make an output directory for .sam files
-mkdir -p results/sam/ 
+## Make output directories for assembly files
+mkdir -p results/sam results/bam results/bcf results/vcf
 
 ## Assemble trimmed sequences against the reference genome, output results to results/sam
 bwa mem data/ref_genome/Halobacterium.fna HaloTrimmed.fastq > results/sam/SRR8699861.aligned.sam
