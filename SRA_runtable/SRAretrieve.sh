95c84efd5c80f3d34233848b82985ab985e994bf
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2022-10-21 11:29:32 -0400

    Update SRAretrieve.sh

--- SRA_runtable/SRAretrieve.sh
+++ SRA_runtable/SRAretrieve.sh
@@ -1,18 +1,20 @@
 ## BTEC495 - Fall 2021. Robinson and B. Lamotte
-## Uses installed SRAtools to prefetch from a list of SRA accessions
+## See the E-Direct query for SraAccList.txt inputs
 
+## Make an sra directory on Storage Volume
 mkdir ../../media/volume/sdb/sra/
 ls ../../media/volume/sdb/sra/
 
+## Fetch SRA data into the volume sra/ directory
 prefetch --option-file SraAccList.txt -O ../../media/volume/sdb/sra/
-
-## prefetch files populate the user home ncbi/ directory
 
 ## for-loop then retreives the full .fastq files for each accession
 
 ## * for file in ncbi/public/sra/*; do fasterq-dump "$file" ; done
 ## cd sra/
 ## fasterq-dump *
+
+## Retrieve .fastq data
 
 
 ## Activate NCBI datasets
