d885836e8c5b66af6c527437740b4dd8ecdf369e
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2022-10-22 10:20:55 -0400

    Update postqc.sh

--- scripts/postqc.sh
+++ scripts/postqc.sh
@@ -1,3 +1,9 @@
 ## Make results directory and transfer fastqc results
-mkdir -p results/fastqc_untrimmed_reads/ mv *.zip results/fastqc_untrimmed_reads/ mv *.html results/fastqc_untrimmed_reads/
+mkdir -p ../../media/volume/sdb/sra/fastq/fastqcresults/
+mkdir -p ../../media/volume/sdb/sra/fastq/fastqcresults/untrimmed
+mkdir -p ../../media/volume/sdb/sra/fastq/fastqcresults/trimmed
+mv ../../media/volume/sdb/sra/fastq/*.zip ../../media/volume/sdb/sra/fastq/fastqcresults/untrimmed/
+mv ../../media/volume/sdb/sra/fastq/*.html ../../media/volume/sdb/sra/fastq/fastqcresults/untrimmed/
+
+#mkdir -p results/fastqc_untrimmed_reads/  mv *.html results/fastqc_untrimmed_reads/
 ##--$ ./postqc.sh  ## Moves fastqc results into qcresults/ directory
